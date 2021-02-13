a27308ecb000e05e0503e79b751801f2c33e899d
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2021-02-13 21:31:37 +0100

    change to css vars, style menu btn

--- src/components/MainTitle/MainTitle.js
+++ src/components/MainTitle/MainTitle.js
@@ -7,7 +7,7 @@ export default function MainTitle() {
     <div className="main-title-container">
       <h1 className="main-title-name uppercase color-change">Josh Hammer</h1>
       <h2 className="main-title-position uppercase">Frontend Developer</h2>
-      <h3 className="main-title-subtitle uppercase">Portfolio</h3>
+      <h3 className="main-title-subtitle uppercase color-change">Portfolio</h3>
     </div>
   )
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,14 +8,15 @@ export default function Navigation() {
     const [isMenuVisible, setMenuVisible] = useState(false);
 
     return(
-        <div className="navigation-sidebar"
-
-            >
-
-            <div className="navigation-menu-button" onClick={() => setMenuVisible(prev => !prev)}></div>
+        <div className="navigation-sidebar">
+            <div className="navigation-menu-button" onClick={() => setMenuVisible(prev => !prev)}>
+                <span></span>
+            </div>
 
             <div className={isMenuVisible ? 'navigation-container visible' : 'navigation-container'}>
-                <div className="navigation-menu-button-black" onClick={() => setMenuVisible(prev => !prev)}></div>
+                <div className="navigation-menu-button-black" onClick={() => setMenuVisible(prev => !prev)}>
+                    <span></span>
+                </div>
                 <nav>
                     <ul className="navigation-list">
                         <li className="navigation-item"><a className="navigation-link" href="/">Home</a></li>
