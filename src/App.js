50ef0e4e088a43bbc301f2a9ef5ccd719a00a6e1
joshhammer <josua.haemmerle@gmx.ch>
2021-02-19 12:25:47 +0100

    fix import path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import Navigation from './components//Navigation/Navigation';
-import LandingPage from './components//LandingPage/LandingPage';
+import LandingPage from './components/LandingPage/LandingPage'
 import './App.scss';
 
 function App() {
