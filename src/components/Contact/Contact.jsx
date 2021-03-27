09848b786477716fe619d22b9020c0e11b6f6b95
joshhammer <josua.haemmerle@gmx.ch>
2021-03-27 16:43:14 +0100

    add basic email form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,46 @@
+
 import React from 'react';
 import {useState} from 'react';
 
 import './Contact.scss';
+// import{ init } from 'emailjs-com';
+// init("user_dfoVy0ZdwGuBathP78bJ7");
 
 export default function Contact() {
   const [firstname, setFirstname] = useState('');
   const [lastname, setLastname] = useState('');
+  const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const templateId = 'contact_template';
+	  sendFeedback(templateId, {
+      message_html: message, 
+      from_name: `${firstname} ${lastname}`, 
+      reply_to: email
+    })
+    initForm();
+  }
+
+  const sendFeedback = (templateId, variables) => {
+    window.emailjs.send(
+      'gmail', 
+      templateId,
+      variables
+      ).then(res => {
+        console.log('Email successfully sent!')
+      })
+      // Handle errors here however you like, or use a React error boundary
+      .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
+    }
+
+  const initForm = () => {
+    setFirstname('');
+    setLastname('');
+    setEmail('');
+    setMessage('');
+  }
 
   return(
     <main className="container contact-container">
@@ -29,10 +62,25 @@ export default function Contact() {
             value={lastname}
             onChange={(event) => setLastname(event.target.value)}
           />
-          <input type="email" name='email' placeholder='Email'/>
+          <input 
+            type="email" 
+            name='email' 
+            placeholder='Email'
+            value={email} 
+            onChange={(event) => setEmail(event.target.value)}
+          />
 
-          <textarea name="" id="" placeholder='Message goes here..'></textarea>
-          <input type="submit" value='Send'/>
+          <textarea 
+            name="message" 
+            id="test-mailing" 
+            placeholder='Message goes here..' 
+            required 
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+          />
+          
+          <input type="submit" value='Send' className='contact-submit-btn' onClick={handleSubmit}/>
+          
         </form>
 
       </div>
